b76536fce9dae362cc12cec5876908c8fbba367e
skinread <dev@kinread.com>
2022-06-21 13:47:56 +1000

    language selector

--- pages/learn.tsx
+++ pages/learn.tsx
@@ -1,4 +1,6 @@
-import { Box, Text, Title } from '@mantine/core';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Box, Button, Text, Title } from '@mantine/core';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 import { AssesmentStatus } from '../components/AssessmentStatus';
@@ -8,16 +10,34 @@ import type { NextPage } from 'next';
 export async function getStaticProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['learningstyle'])),
+      ...(await serverSideTranslations(locale, ['common', 'learningstyle'])),
       // Will be passed to the page component as props
     },
   };
 }
 
+const LanguageSwitcher = ({ currentLocale }: { currentLocale: string }) => (
+  <Box sx={{ marginTop: 16 }}>
+    {currentLocale.includes('en') && (
+      <Link href="/learn" locale="fr">
+        <Button variant="light">Français</Button>
+      </Link>
+    )}
+    {currentLocale.includes('fr') && (
+      <Link href="/learn" locale="en">
+        <Button variant="light">English</Button>
+      </Link>
+    )}
+  </Box>
+);
+
 const Learn: NextPage = () => {
+  const router = useRouter();
   const { t } = useTranslation();
+
   return (
     <>
+      <LanguageSwitcher currentLocale={router.locale} />
       <AssesmentStatus />
       <Title order={1} sx={{ marginBottom: '.5em' }}>
         {t('learningstyle:title')}

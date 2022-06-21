24af18c79fab5573e684b932dc853d09035848cf
skinread <dev@kinread.com>
2022-06-21 12:45:31 +1000

    correct next-i18next dep

--- pages/learn.tsx
+++ pages/learn.tsx
@@ -15,21 +15,16 @@ export async function getStaticProps({ locale }) {
 }
 
 const Learn: NextPage = () => {
+  const { t } = useTranslation();
   return (
     <>
       <AssesmentStatus />
       <Title order={1} sx={{ marginBottom: '.5em' }}>
-        Learning style
+        {t('learningstyle:title')}
       </Title>
-      <Text size="sm">
-        This WithYouWithMe test will help you better understand which way you
-        like to learn. We all learn differently - some of us prefer to learn
-        through touch, others through sound or sight. This test will help
-        highlight what’s your individual style.
-      </Text>
+      <Text size="sm">{t('learningstyle:intro-a')}</Text>
       <Text size="sm" sx={{ marginTop: '.5em' }}>
-        It is strongly recommended that you are in a quiet environment to avoid
-        distractions and use a stable Internet connection when taking tests.
+        {t('learningstyle:intro-b')}
       </Text>
       <Box component="main" sx={{ marginTop: 24 }}>
         <LearningStyle />
